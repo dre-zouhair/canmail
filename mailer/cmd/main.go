aa243eae8e9d352cd93e2b913dd693b18398daac
DRIOUECHE Zouhair <dre.zouhair@gmail.com>
2023-03-05 12:07:53 +0100

    Update main file to open and close redis connection

--- mailer/cmd/main.go
+++ mailer/cmd/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/dre-zouhair/mailer/internal/db"
 )
 
 func main() {
-	fmt.Println("canMail !")
+	var connection, err = db.Connect()
+	if err != nil {
+		fmt.Println("unable to init connection with redis db :", err)
+		return
+	} else {
+		fmt.Println("Connected ", connection.GetDB().ClientID())
+	}
+
+	defer closeConnect(connection)
+}
+
+func closeConnect(connection *db.Database) {
+	err := connection.Close()
+	if err != nil {
+		fmt.Println("unable to close redis connection")
+		return
+	} else {
+		fmt.Println("Disconnected")
+	}
 }
