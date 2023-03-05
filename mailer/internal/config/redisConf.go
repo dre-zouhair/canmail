644caba7043ccde14734f8bdf7ca6cca77d813e7
DRIOUECHE Zouhair <dre.zouhair@gmail.com>
2023-03-05 14:45:57 +0100

    Refactor

--- mailer/internal/config/redisConf.go
+++ mailer/internal/config/redisConf.go
@@ -14,28 +14,25 @@ type RedisConf struct {
 func NewRedisConf() *RedisConf {
 	fmt.Println(os.Environ())
 	redisHost := os.Getenv("REDIS_HOST")
-	fmt.Println("REDIS_HOST ", redisHost)
 	if redisHost == "" {
 		fmt.Println("redisHost is not set")
 		return nil
 	}
 	redisPassword := os.Getenv("REDIS_PASSWORD")
-	fmt.Println("redisPassword ", redisPassword)
 	if redisPassword == "" {
 		fmt.Println("REDIS_PASSWORD is not set")
 		return nil
 	}
 
 	redisDB := os.Getenv("REDIS_DB")
-	fmt.Println("REDIS_DB ", redisDB)
 	if redisDB == "" {
-		fmt.Println("redisPassword is not set")
+		fmt.Println("REDIS_DB is not set")
 		return nil
 	}
 
 	dbNumber, err := strconv.Atoi(redisDB)
 	if err != nil {
-		fmt.Println("redisDB does not have a valid value")
+		fmt.Println("REDIS_DB does not have a valid value")
 		return nil
 	}
 
