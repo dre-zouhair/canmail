b00b5c4e0678116681fb5d69267e2a0e34f65a0b
DRIOUECHE Zouhair <dre.zouhair@gmail.com>
2023-03-21 23:23:34 +0100

    Added ConcurrentBulk

--- mailer/service/bulk.go
+++ mailer/service/bulk.go
@@ -46,3 +46,35 @@ func GetMailServer() (*MailServer, error) {
 	mailServer := NewMailServer("", mailConf.Host, mailConf.Domain, mailConf.GetFromConf().FromAddress, mailConf.GetFromConf().FromPassword)
 	return mailServer, nil
 }
+
+func (server *MailServer) ConcurrentBulk(targets []model.Target, template *model.Template, success chan model.Target, fails chan model.Target) {
+	if len(targets) < 50 {
+		go server.chunk(targets, template, success, fails)
+		return
+	}
+
+	i := 0
+	for ; i < len(targets); i = i + 50 {
+		go server.chunk(targets[i:i+50], template, success, fails)
+	}
+
+	go server.chunk(targets[i-50:], template, success, fails)
+
+}
+
+func (server *MailServer) chunk(targets []model.Target, template *model.Template, success chan model.Target, fails chan model.Target) {
+	for _, target := range targets {
+		templateBody := template.Build(target.Model)
+		mail := Mail{
+			To:      []string{target.Email},
+			Subject: template.Subject,
+			Body:    templateBody,
+		}
+		err := server.SendMail(mail)
+		if err != nil {
+			fails <- target
+		} else {
+			success <- target
+		}
+	}
+}
